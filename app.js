d2cff780d0ff1dc19255384e2b007ffc2e77de4c
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2021-06-11 05:13:54 -0300

    port 80

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 3000
+const PORT = 80
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json()) // To parse the incoming requests with JSON payloads
