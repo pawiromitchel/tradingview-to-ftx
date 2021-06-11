1f79d78085379c48ab4aa289b9a634b00e174092
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2021-06-11 05:05:06 -0300

    add /

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const PORT = 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 
+app.get("/", (req, res) => {
+    res.status(200).send('silence is golden').end()
+})
+
 app.post("/hook", (req, res) => {
     console.log(req.body)
     res.status(200).end()
