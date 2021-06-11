c37a98a2be332e4ed816e40b972ad5167f7da29b
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2021-06-11 16:21:24 -0300

    handle json and text

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const app = express()
+// To parse the incoming requests with JSON payloads
+app.use(express.urlencoded({extended: true}))
+app.use(express.text())
+app.use(express.json())
+
+/**
+ * Made possible by forwarding port 80 from node to the server
+ * https://www.digitalocean.com/community/tutorials/how-to-use-pm2-to-setup-a-node-js-production-environment-on-an-ubuntu-vps
+ */
 const PORT = 80
 
 app.use(express.urlencoded({ extended: true }))
