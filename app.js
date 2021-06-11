06925515c679e3f85ac30d82048bc0c70833b5cb
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2021-06-11 04:59:32 -0300

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
+app.post("/hook", (req, res) => {
+    console.log(req.body)
+    res.status(200).end()
+})
+
+// Start express on the defined port
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
